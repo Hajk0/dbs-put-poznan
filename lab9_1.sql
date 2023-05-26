666916dcc7714c5063a004ad26e75709e05ad399
Witold Hajkowicz <hajko2801@gmail.com>
2023-05-26 14:15:14 +0200

    lab9 and reset database files updated

--- lab9_1.sql
+++ lab9_1.sql
@@ -1,3 +1,4 @@
+-- 1
 CREATE TABLE projekty (
     id_projektu NUMBER(4, 0) GENERATED ALWAYS AS IDENTITY,
     opis_projektu VARCHAR2(20),
@@ -6,22 +7,35 @@ CREATE TABLE projekty (
     fundusz NUMBER(7, 2)
 );
 
+-- 2
 INSERT INTO projekty(opis_projektu, data_rozpoczecia, data_zakonczenia, fundusz)
     VALUES ('Indeksy bitmapowe', DATE '1999-04-02', DATE '2001-08-31', 25000);
 INSERT INTO projekty(opis_projektu, fundusz)
     VALUES ('Sieci kręgosłupowe', 19000);
 
+-- 3
 SELECT id_projektu, opis_projektu FROM projekty;
 
+-- 4
+-- INSERT INTO projekty(id_projektu, opis_projektu, data_rozpoczecia, data_zakonczenia, fundusz) VALUES (10, 'Indeksy drzewiaste', DATE '2013-12-24', DATE '2014-01-01', 1200);
 INSERT INTO projekty(opis_projektu, data_rozpoczecia, data_zakonczenia, fundusz)
     VALUES ('Indeksy drzewiaste', DATE '2013-12-24', DATE '2014-01-01', 1200);
 
-CREATE TABLE projekty_kopia AS
-    SELECT * FROM PROJEKTY;
+-- 5
+-- UPDATE projekty SET id_projektu = 10 WHERE opis_projektu = 'Indeksy drzewiaste';
 
+-- 6
+CREATE TABLE projekty_kopia AS
+    SELECT * FROM projekty;
+SELECT * FROM projekty_kopia;
+
+-- 7
 INSERT INTO projekty_kopia(id_projektu, opis_projektu, data_rozpoczecia, data_zakonczenia, fundusz)
     VALUES (10, 'Sieci lokalne', SYSDATE, ADD_MONTHS(SYSDATE, 12), 24500);
 
-DELETE FROM PROJEKTY WHERE opis_projektu LIKE '%drzew%';
+-- 8
+DELETE FROM projekty WHERE opis_projektu = 'Indeksy drzewiaste';
+SELECT * FROM projekty_kopia;
 
+-- 9
 SELECT TABLE_NAME FROM USER_TABLES ORDER BY TABLE_NAME;
